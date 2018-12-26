7d6142092da4f1886d586c42b1c3d13d0bb9fefc
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-26 15:14:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-draw();
+draw1();
 var players=[0,0,0,0];
 for(var i=0;i<4;i++){
 players[i]=new player("","classic",i);
 }
-function draw(){
+function draw1(){
+players[0].draw();
+if(keys.up)
+players[0].y-=1;
+if(keys.dowm)
+players[0].y+=1;
+if(keys.left)
+players[0].x-=1;
+if(keys.right)
+players[0].x+=1;
 window.requestAnimationFrame(draw);
 }
