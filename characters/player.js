f0612b4735e7f5033a4212bbfdacc2ccfb407deb
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-27 13:14:34 -0600

    Update player.js

--- characters/player.js
+++ characters/player.js
@@ -1,4 +1,4 @@
-function player(name,cls,id){
+function player(name,char,id){
   this.name=name;
   this.color="#FF0000";
   this.x=0;
@@ -10,9 +10,41 @@ function player(name,cls,id){
   this.health=100;
   this.class=cls;
   this.id=id;
+  this.move=false;
   this.powerUps=[0,0,0,0,0,0,0];//high jump,dbl jump,flight,ex health,sheild,dbl coins,speed boost
   this.draw=function(){
     ctx.fillStyle=this.color;
     ctx.fillRect(this.x,this.y,20,20);
+    ctx.fillStyle="#FFFFFF";
+    ctx.arc(this.x+10,this.y+10,7,0,360);
+    ctx.fillStyle="#000000";
+    if(this.move!==false){
+      if(this.move==="left")
+        ctx.arc(this.x+4,this.y+10,3,0,360);
+      if(this.move==="right")
+        ctx.arc(this.x+11,this.y+10,3,0,360);
+      if(this.move==="up")
+        ctx.arc(this.x+10,this.y+4,3,0,360);
+      if(this.move==="down")
+        ctx.arc(this.x+10,this.y+11,3,0,360);
+    }else{
+      ctx.arc(this.x+10,this.y+10,3,0,360);
+    }
+  }
+  this.left=function(){
+    this.move="left";
+    this.x--;
+  }
+  this.right=function(){
+    this.move="right";
+    this.x++;
+  }
+  this.up=function(){
+    this.move="up";
+    this.y--;
+  }
+  this.down=function(){
+    this.move="down";
+    this.y++;
   }
 }
