1c7d5db13dcb81d6c8352b12e5c88c684cddc040
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-27 12:59:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ players[i]=new player("","classic",i);
 function LEVEL1(){
 ctx.clearRect(0,0,c.width,c.height);
 if(keys.up)
-players[0].y-=1;
+players[0].jump();
+/*
 if(keys.down)
 players[0].y+=1;
+*/
 if(keys.left)
-players[0].x-=1;
+players[0].left()
 if(keys.right)
-players[0].x+=1;
+players[0].right();
+if(!keys.left&&!keys.right&&!keys.up&&!keys.down)
+players[0].stay();
 players[0].draw();
 window.requestAnimationFrame(LEVEL1);
 }
