833fa05aa14f066658a56f06e364b218a8e9a0fb
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-28 12:51:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-var players,AOP,level,coords;
+var players,AOP,level,coords,uC;
 function LEVELx(){
-players=[];
-players.length=4;
-AOP=1;
-level=["0000000000","0........0","0........0","0........0","0~~~~~~~~0","0000000000","0000000000","0........0","0.@.......0","0000000000"];
-for(var i=0;i<level.length;i++){
-  for(var ii=0;ii<level[i].length;ii++){
-    if(level[i].charAt(ii)=="@")
-      coords=[ii,i];
+  uC=0;
+  players=[];
+  players.length=4;
+  AOP=1;
+  level=["0000000000","0........0","0........0","0........0","0~~~~~~~~0","0000000000","0000000000","0........0","0.@.......0","0000000000"];
+  for(var i=0;i<level.length;i++){
+    for(var ii=0;ii<level[i].length;ii++){
+      if(level[i].charAt(ii)=="@")
+        coords=[ii,i];
+    }
   }
-}
 
-for(var i=0;i<players.length;i++){
-  players[i]=new player("","classic",i);
-  players[i].x=coords[0]*(c.width/10);
-  players[i].y=coords[1]*(c.height/10);
-}
+  for(var i=0;i<players.length;i++){
+    players[i]=new player("","classic",i);
+    players[i].x=coords[0]*(c.width/10);
+    players[i].y=coords[1]*(c.height/10);
+  }
   LEVEL();
 }
 function LEVEL(){
   ctx.clearRect(0,0,c.width,c.height);
+  if(keys.up)
+    players[0].jC++;
+  if(!keys.up)
+    players[0].jC=0;
   for(var i=0;i<AOP;i++){
     if(keys.left&&collision(players[i])[0])
-      players[i].left();
+      players[0].left();
     if(keys.right&&collision(players[i])[1])
-      players[i].right();
-    if(keys.up&&collision(players[i])[2])
-      players[i].up();
+      players[0].right();
+    if(keys.up&&collision(players[i])[2]&&jumpTest(players[i]))
+      players[0].up();
     players[i].draw();
   }
   window.requestAnimationFrame(LEVEL1);
@@ -34,13 +39,13 @@ function LEVEL(){
 function collision1(player){
   var cols=[true,true,true,true];
   var sec=c.width/level[0].length;
-  if(level[Math.trunc(player.y/sec)].charAt(Math.trunc((player.x/sec))-1)=="0")
+  if(level[Math.trunc(player.y/sec)].charAt(Math.trunc((player.x-1)/sec))=="0")
     cols[0]=false;
-  if(level[Math.trunc(player.y/sec)].charAt(Math.trunc((player.x/sec)+1))=="0")
+  if(level[Math.trunc(player.y/sec)].charAt(Math.trunc((player.x+1)/sec))=="0")
     cols[1]=false;
-  if(level[Math.trunc((player.y/sec)-1)].charAt(Math.trunc(player.x/sec)=="0")
+  if(level[Math.trunc((player.y-1)/sec)].charAt(Math.trunc(player.x/sec))=="0")
     cols[2]=false;
-  if(level[Math.trunc((player.y/sec)+1)].charAt(Math.trunc(player.x/sec))=="0")
+  if(level[Math.trunc((player.y+1)/sec)].charAt(Math.trunc(player.x/sec))=="0")
     cols[3]=false;
   return cols;
 }
