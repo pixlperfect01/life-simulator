7c9d88e2422348f96eea93d45323a996fa4023cf
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-29 09:30:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,21 @@
-var players,AOP,level,coords,uC;
-function LEVELx(){
-  uC=0;
-  players=[];
-  players.length=4;
-  AOP=1;
-  level=["00000000000000000000","0..................0","0..................0","0..................0","0..................0","00000000000000000000","00000000000000000000","0..................0","0...............@...0","00000000000000000000"];
+var c,ctx,level;
+function menuMain(){
+  c=document.getElementById("myCanvas");
+  ctx=c.getContext("2d");
+  level=["0000000000000000000000000","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0.......................0","0000000000000000000000000"];
+  play();
+}
+
+function level(){
+  drawLevel();
+  window.requestAnimationFrame(level);
+}
+
+function drawLevel(){
   for(var i=0;i<level.length;i++){
     for(var ii=0;ii<level[i].length;ii++){
-      if(level[i].charAt(ii)=="@")
-        coords=[ii,i];
+      ctx.fillStyle="#000000";
+      ctx.fillRect(ii*25,i*25,20,20);
     }
   }
-
-  for(var i=0;i<players.length;i++){
-    players[i]=new player("","classic",i);
-    players[i].x=coords[0]*(15);
-    players[i].y=coords[1]*(15);
-  }
-  LEVEL();
-}
-function LEVEL(){
-  ctx.clearRect(0,0,c.width,c.height);
-  for(var i=0;i<AOP;i++){
-  if(eval(players[i].upKey))
-    players[i].jC++;
-  if(!eval(players[i].upKey))
-    players[i].jC=0;
-    if(eval(players[i].leftKey)&&collision(players[i])[0])
-      players[0].left();
-    if(eval(players[i].rightKey)&&collision(players[i])[1])
-      players[0].right();
-    if(eval(players[i].upKey)&&collision(players[i])[2]&&jumpTest(players[i]))
-      players[0].up();
-    players[i].draw();
-  }
-  window.requestAnimationFrame(LEVEL);
-}
-function collision(player){
-  var cols=[true,true,true,true];
-  var sec=15;
-  if(level[Math.trunc(player.y/sec)].charAt(Math.trunc((player.x-1)/sec))=="0")
-    cols[0]=false;
-  if(level[Math.trunc(player.y/sec)].charAt(Math.trunc((player.x+1)/sec))=="0")
-    cols[1]=false;
-  if(level[Math.trunc((player.y-1)/sec)].charAt(Math.trunc(player.x/sec))=="0")
-    cols[2]=false;
-  if(level[Math.trunc((player.y+1)/sec)].charAt(Math.trunc(player.x/sec))=="0")
-    cols[3]=false;
-  return cols;
-}
-function jumpTest(player){
-  
-  return true;
 }
